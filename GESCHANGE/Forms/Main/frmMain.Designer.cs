36ce8ca3080fcffdaef1a4e8c98d9ceded33d8dc
Ilyasse <Ilyasse@ILYASSE-PC>
2022-09-18 18:54:09 +0100

    Clean Codes

--- GESCHANGE/Forms/Main/frmMain.Designer.cs
+++ GESCHANGE/Forms/Main/frmMain.Designer.cs
@@ -124,6 +124,7 @@
             this.Controls.Add(this.pnlMenu);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Trebuchet MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MinimumSize = new System.Drawing.Size(900, 600);
