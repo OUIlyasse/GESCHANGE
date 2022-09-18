49f1934f6c8044c6e4c3299e3366c31c7c637aed
Ilyasse <Ilyasse@ILYASSE-PC>
2022-09-18 13:31:42 +0100

    Clean Codes

--- GESCHANGE/Forms/Sub/frmSNew.cs
+++ GESCHANGE/Forms/Sub/frmSNew.cs
@@ -1,12 +1,6 @@
 ﻿using GESCHANGE.Db;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using static Tools.iTools;
 
